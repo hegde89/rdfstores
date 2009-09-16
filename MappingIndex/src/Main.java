f54cf4128bbd6740a69d743657d8fefe9f02e151
cheger2 <cheger2@ef0b77ad-2f51-0410-a52a-bb8a362cb4f1>
2009-09-16 16:21:40 +0000

    Option -s and -d added to main and MappingIndexCreator changed to comply with it.

---
+++ MappingIndex/src/Main.java
@@ -0,0 +1,57 @@
+import java.util.List;
+
+import edu.unika.aifb.graphindex.importer.Importer;
+import edu.unika.aifb.graphindex.importer.NxImporter;
+import edu.unika.aifb.graphindex.index.IndexDirectory;
+
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
+public class Main {
+
+	/**
+	 * @param args mapping file(s)
+	 */	
+	
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+
+			OptionParser op = new OptionParser();
+			
+			// Define option for output directory
+			op.accepts("o", "output directory").withRequiredArg().ofType(String.class).describedAs("directory");
+			// Define option for the data source used as source in the mapping
+			op.accepts("s", "source datasource").withRequiredArg();
+			// Define option for the data source used as destination in the mapping
+			op.accepts("d", "destination datasource").withRequiredArg();
+			
+			// Parse arguments with option parser
+			OptionSet os = op.parse(args);
+			
+			// Check for output directory in option set
+			if (!os.has("o") || os.nonOptionArguments().size() == 0) {
+				op.printHelpOn(System.out);
+				return;
+			}
+			
+			// Get output directory
+			String directory = (String)os.valueOf("o");
+			// Get source
+			String source = (String)os.valueOf("s");
+			// Get destination
+			String destination = (String)os.valueOf("d");
+			
+			// Get filename
+			List<String> files = os.nonOptionArguments();
+
+			Importer importer = new NxImporter();
+			importer.addImports(files);
+
+			MappingIndexCreator mic = new MappingIndexCreator(new IndexDirectory(directory), source, destination);
+			
+			mic.setImporter(importer);
+			mic.setCreateDataIndex(true);
+			mic.create();
+	}
+
+}

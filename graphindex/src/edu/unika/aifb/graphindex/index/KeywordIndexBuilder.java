d06b219ab54b41128497d843f4cac69fbd301174
gladwig <gladwig@gmail.com>
2009-09-19 08:10:55 +0000

    switch to Lucene 2.4.1

--- graphindex/src/edu/unika/aifb/graphindex/index/KeywordIndexBuilder.java
+++ graphindex/src/edu/unika/aifb/graphindex/index/KeywordIndexBuilder.java
@@ -25,8 +25,10 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
@@ -73,11 +75,11 @@ public class KeywordIndexBuilder {
 	/*
 	 * False positives will happen with probability 2<sup>-<var>NUMBER_HASHFUNCTION</var></sup>
 	 * */
-	private static int NUMBER_HASHFUNCTION = 5;
 	private static double FALSE_POSITIVE = 0.001;
 
 	private IndexDirectory idxDirectory;
-	private DataIndex dataIndex;
+	private IndexConfiguration idxConfig;
+	protected DataIndex dataIndex;
 	private BlockCache  blockSearcher;
 	private NeighborhoodStorage ns;
 	
@@ -85,6 +87,7 @@ public class KeywordIndexBuilder {
 	
 	public KeywordIndexBuilder(IndexDirectory idxDirectory, IndexConfiguration idxConfig) throws IOException, StorageException {
 		this.idxDirectory = idxDirectory;
+		this.idxConfig = idxConfig;
 		this.dataIndex = new DataIndex(idxDirectory, idxConfig);
 		
 		this.ns = new LuceneNeighborhoodStorage(idxDirectory.getDirectory(IndexDirectory.NEIGHBORHOOD_DIR, true));
@@ -92,12 +95,13 @@ public class KeywordIndexBuilder {
 		
 		HOP = idxConfig.getInteger(IndexConfiguration.KW_NSIZE);
 		
-		try {
-			this.blockSearcher = new BlockCache(idxDirectory);
-		} catch (DatabaseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		};	
+		if (idxConfig.getBoolean(IndexConfiguration.HAS_SP)) {
+			try {
+				this.blockSearcher = new BlockCache(idxDirectory);
+			} catch (DatabaseException e) {
+				e.printStackTrace();
+			}
+		}	
 	}
 
 	public void indexKeywords() throws StorageException, IOException {
@@ -107,18 +111,24 @@ public class KeywordIndexBuilder {
 			StandardAnalyzer analyzer = new StandardAnalyzer();
 			IndexWriter indexWriter = new IndexWriter(indexDir, analyzer,true);
 			indexWriter.setMaxFieldLength(MAXFIELDLENGTH);
+			
 			log.info("Indexing concepts");
 			indexSchema(indexWriter, idxDirectory.getTempFile("concepts", false), TypeUtil.CONCEPT, CONCEPT_BOOST);
+			
 			log.info("Indexing attributes");
 			indexSchema(indexWriter, idxDirectory.getTempFile("attributes", false), TypeUtil.ATTRIBUTE, ATTRIBUTE_BOOST);
+			
 			log.info("Indexing relations");
 			indexSchema(indexWriter, idxDirectory.getTempFile("relations", false), TypeUtil.RELATION, RELATION_BOOST);	
+			
 			log.info("Indexing entities");
 			indexEntity(indexWriter, idxDirectory.getTempFile("entities", false));
+			
 			indexWriter.optimize();
 			indexWriter.close();
 			
-			blockSearcher.close();
+			if (blockSearcher != null)
+				blockSearcher.close();
 			ns.optimize();
 			ns.close();
 		} 
@@ -180,7 +190,7 @@ public class KeywordIndexBuilder {
 				doc.add(new Field(Constant.URI_FIELD, uri, Field.Store.YES, Field.Index.UN_TOKENIZED));
 				
 				// indexing extension id for concept
-				if(type.equals(TypeUtil.CONCEPT)){
+				if(type.equals(TypeUtil.CONCEPT) && idxConfig.getBoolean(IndexConfiguration.HAS_SP)) {
 					String blockName = blockSearcher.getBlockName(uri);
 					doc.add(new Field(Constant.EXTENSION_FIELD, blockName, Field.Store.YES, Field.Index.NO));
 				}
@@ -194,104 +204,107 @@ public class KeywordIndexBuilder {
 		}
 	}
 	
+	protected List<Field> getFieldsForEntity(String uri) throws IOException, StorageException {
+		List<Field> fields = new ArrayList<Field>();
+		String localName = TypeUtil.getLocalName(uri).trim();
+		
+		// indexing type of the element
+		fields.add(new Field(Constant.TYPE_FIELD, TypeUtil.ENTITY, Field.Store.YES, Field.Index.NO));
+		
+		// indexing local name
+		Field field = new Field(Constant.LOCALNAME_FIELD, localName, Field.Store.YES, Field.Index.TOKENIZED);
+		field.setBoost(ENTITY_DISCRIMINATIVE_BOOST);
+		fields.add(field);
+		
+		// indexing uri
+		fields.add(new Field(Constant.URI_FIELD, uri, Field.Store.YES, Field.Index.UN_TOKENIZED));
+		
+		// indexing extension id 
+		if (idxConfig.getBoolean(IndexConfiguration.HAS_SP)) {
+			String blockName = blockSearcher.getBlockName(uri);
+			fields.add(new Field(Constant.EXTENSION_FIELD, blockName, Field.Store.YES, Field.Index.NO));
+		}
+		
+		// indexing concept of the entity element
+		Set<String> concepts = computeConcepts(uri);
+		if(concepts != null && concepts.size() != 0) {
+			for(String concept : concepts) {
+				field = new Field(Constant.CONCEPT_FIELD, concept, Field.Store.YES, Field.Index.UN_TOKENIZED);
+				field.setBoost(ENTITY_DISCRIMINATIVE_BOOST);
+				fields.add(field);
+			}
+		}
+		
+		// indexing label
+		Set<String> labels = computeLabels(uri);
+		if(labels != null && labels.size() != 0) {
+			for(String label : labels){
+				field = new Field(Constant.LABEL_FIELD, label, Field.Store.YES, Field.Index.TOKENIZED);
+				field.setBoost(ENTITY_DISCRIMINATIVE_BOOST);
+				fields.add(field);
+			} 
+		}	
+		
+		// indexing attribute-value and relation-entityID compounds
+		Set<String> compounds = computeEntityDescriptions(uri);
+		if(compounds != null && compounds.size() != 0) {
+			for(String compound : compounds){
+				String[] str = compound.trim().split(SEPARATOR);
+				String attributeOrRelation,valueOrEntitiyId;
+				if(str.length == 2) {
+					attributeOrRelation = str[1];
+					valueOrEntitiyId = str[0]; 
+				}
+				else {
+					continue;
+				}
+				
+				field = new Field(attributeOrRelation, valueOrEntitiyId, Field.Store.YES, Field.Index.TOKENIZED);
+				field.setBoost(ENTITY_DESCRIPTIVE_BOOST);
+				fields.add(field);
+			} 
+		}	
+		
+		// indexing reachable entities
+		Set<String> reachableEntities = computeReachableEntities(uri);
+//		BloomFilter bf = new BloomFilter(reachableEntities.size(), NUMBER_HASHFUNCTION);
+		BloomFilter bf = new BloomFilter(reachableEntities.size(), FALSE_POSITIVE);
+		for(String entity : reachableEntities){
+			if(entity.startsWith("http://www."))
+				entity = entity.substring(11);
+			else if(entity.startsWith("http://"))
+				entity = entity.substring(7);
+			bf.add(entity);
+		} 
+//		ByteArrayOutputStream byteArrayOut = new ByteArrayOutputStream();
+//		ObjectOutputStream objectOut = new ObjectOutputStream(byteArrayOut);
+//		objectOut.writeObject(bf);
+//		byte[] bytes = byteArrayOut.toByteArray(); 
+//		doc.add(new Field(Constant.NEIGHBORHOOD_FIELD, bytes, Field.Store.YES));
+
+		ns.addNeighborhoodBloomFilter(uri, bf);
+		
+		return fields;
+	}
+	
 	public void indexEntity(IndexWriter indexWriter, File file) throws IOException, StorageException {
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(file));
 			String line;
 			int entities = 0;
 			while ((line = br.readLine()) != null) {
-				String tokens [] = line.split("\t");
-				String uri,localName;
-				if(tokens.length == 1) {
-					uri = line.trim();
-					localName = TypeUtil.getLocalName(uri).trim(); 
-				}
-				else {
-					continue;
-				}
+				String uri = line.trim();
 				
-//				log.info("Indexing entity " + uri);
-				
-				localName = localName.toLowerCase();
-				/* Write Index */
 				Document doc = new Document();
-				
-				// indexing type of the element
-				doc.add(new Field(Constant.TYPE_FIELD, TypeUtil.ENTITY, Field.Store.YES, Field.Index.NO));
-				
-				// indexing local name
-				Field field = new Field(Constant.LOCALNAME_FIELD, localName, Field.Store.YES, Field.Index.TOKENIZED);
-				field.setBoost(ENTITY_DISCRIMINATIVE_BOOST);
-				doc.add(field);
-				
-				// indexing uri
-				doc.add(new Field(Constant.URI_FIELD, uri, Field.Store.YES, Field.Index.UN_TOKENIZED));
-				
-				// indexing extension id 
-				String blockName = blockSearcher.getBlockName(uri);
-				doc.add(new Field(Constant.EXTENSION_FIELD, blockName, Field.Store.YES, Field.Index.NO));
-				
-				// indexing concept of the entity element
-				Set<String> concepts = computeConcepts(uri);
-				if(concepts != null && concepts.size() != 0) {
-					for(String concept : concepts) {
-						field = new Field(Constant.CONCEPT_FIELD, concept, Field.Store.YES, Field.Index.UN_TOKENIZED);
-						field.setBoost(ENTITY_DISCRIMINATIVE_BOOST);
-						doc.add(field);
-					}
-				}
-				
-				// indexing label
-				Set<String> labels = computeLabels(uri);
-				if(labels != null && labels.size() != 0) {
-					for(String label : labels){
-						field = new Field(Constant.LABEL_FIELD, label, Field.Store.YES, Field.Index.TOKENIZED);
-						field.setBoost(ENTITY_DISCRIMINATIVE_BOOST);
-						doc.add(field);
-					} 
-				}	
-				
-				// indexing attribute-value and relation-entityID compounds
-				Set<String> compounds = computeEntityDescriptions(uri);
-				if(compounds != null && compounds.size() != 0) {
-					for(String compound : compounds){
-						String[] str = compound.trim().split(SEPARATOR);
-						String attributeOrRelation,valueOrEntitiyId;
-						if(str.length == 2) {
-							attributeOrRelation = str[1];
-							valueOrEntitiyId = str[0]; 
-						}
-						else {
-							continue;
-						}
-						
-						field = new Field(attributeOrRelation, valueOrEntitiyId, Field.Store.YES, Field.Index.TOKENIZED);
-						field.setBoost(ENTITY_DESCRIPTIVE_BOOST);
-						doc.add(field);
-					} 
-				}	
-				
-				// indexing reachable entities
-				Set<String> reachableEntities = computeReachableEntities(uri);
-//				BloomFilter bf = new BloomFilter(reachableEntities.size(), NUMBER_HASHFUNCTION);
-				BloomFilter bf = new BloomFilter(reachableEntities.size(), FALSE_POSITIVE);
-				for(String entity : reachableEntities){
-					if(entity.startsWith("http://www."))
-						entity = entity.substring(11);
-					else if(entity.startsWith("http://"))
-						entity = entity.substring(7);
-					bf.add(entity);
-				} 
-//				ByteArrayOutputStream byteArrayOut = new ByteArrayOutputStream();
-//				ObjectOutputStream objectOut = new ObjectOutputStream(byteArrayOut);
-//				objectOut.writeObject(bf);
-//				byte[] bytes = byteArrayOut.toByteArray(); 
-//				doc.add(new Field(Constant.NEIGHBORHOOD_FIELD, bytes, Field.Store.YES));
-
-				ns.addNeighborhoodBloomFilter(uri, bf);
-				
 				doc.setBoost(ENTITY_BOOST);
+				
+				List<Field> fields = getFieldsForEntity(uri);
+				for (Field f : fields)
+					doc.add(f);
+				
 				indexWriter.addDocument(doc);
+				
+				indexWriter.commit();
 				
 				entities++;
 				if (entities % 1000000 == 0)
